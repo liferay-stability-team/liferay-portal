18faeed829fc1651794d62081db964064c51d2d0
Akhash R <akhash.ramprakash@liferay.com>
2026-04-02 14:57:58 +0530

    LPD-84862 Reuse existing method

--- modules/apps/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/internal/servlet/FriendlyURLServlet.java
+++ modules/apps/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/internal/servlet/FriendlyURLServlet.java
@@ -567,9 +567,7 @@ public class FriendlyURLServlet extends HttpServlet {
 
 		Layout layout = (Layout)httpServletRequest.getAttribute(WebKeys.LAYOUT);
 
-		if ((layout != null) &&
-			Objects.equals(layout.getType(), LayoutConstants.TYPE_URL)) {
-
+		if ((layout != null) && layout.isTypeURL()) {
 			String paramString = HttpComponentsUtil.parameterMapToString(
 				params, !actualURL.contains(StringPool.QUESTION));
 
